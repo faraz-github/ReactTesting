4541f8406dc30fc96bcfd2db4daa64c4abec388b
faraz-github <farazahmad.email@gmail.com>
2023-05-09 23:55:52 +0530

    test coverage, coverage scriptsm threshold report

--- reactjs_ts_test/src/components/greet/greet.test.tsx
+++ reactjs_ts_test/src/components/greet/greet.test.tsx
@@ -6,24 +6,36 @@ import { Greet } from "./greet";
 // Describe is used to group test
 // It can be used parallel and nested
 // Similarly describe can also use describe.only and describe.skip
+// describe("Greet", () => {
+//   test("Renders correctly", () => {
+//     render(<Greet />);
+//     const textElement = screen.getByText("Hello");
+//     expect(textElement).toBeInTheDocument();
+//   });
+
+//   // describe can also be nested
+//   describe("Nested", () => {
+//     // Only and skip
+//     // 1. if in a file we have more than 1 test and we add
+//     // 2. test.only then only that perticular test will be done
+//     // 3. if we use test.skip that perticular test will be skipped
+
+//     test("Renders with a name", () => {
+//       render(<Greet name="Faraz" />);
+//       const textElement = screen.getByText("Hello Faraz");
+//       expect(textElement).toBeInTheDocument();
+//     });
+//   });
+// });
+
+
+// For coverage Threshold
 describe("Greet", () => {
   test("Renders correctly", () => {
     render(<Greet />);
-    const textElement = screen.getByText("Hello");
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 
-  // describe can also be nested
-  describe("Nested", () => {
-    // Only and skip
-    // 1. if in a file we have more than 1 test and we add
-    // 2. test.only then only that perticular test will be done
-    // 3. if we use test.skip that perticular test will be skipped
-
-    test("Renders with a name", () => {
-      render(<Greet name="Faraz" />);
-      const textElement = screen.getByText("Hello Faraz");
-      expect(textElement).toBeInTheDocument();
-    });
-  });
+ 
 });
