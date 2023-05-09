864635c2cd9d7f2d640c7c6f02c50ff467c20a1e
faraz-github <farazahmad.email@gmail.com>
2023-05-09 23:06:35 +0530

    only and skip test

--- reactjs_ts_test/src/components/greet/greet.test.tsx
+++ reactjs_ts_test/src/components/greet/greet.test.tsx
@@ -7,6 +7,11 @@ test("Greet renders correctly", () => {
   expect(textElement).toBeInTheDocument();
 });
 
+// Only and skip
+// 1. if in a file we have more than 1 test and we add
+// 2. test.only then only that perticular test will be done
+// 3. if we use test.skip that perticular test will be skipped
+
 test("Greet renders with a name", () => {
   render(<Greet name="Faraz" />);
   const textElement = screen.getByText("Hello Faraz");
