b792a4ba616a28edc0464d5f090ad1d14d8cc2da
faraz-github <farazahmad.email@gmail.com>
2023-05-26 23:55:01 +0530

    Setup for testing components with providers

--- reactjs_ts_test/src/App.tsx
+++ reactjs_ts_test/src/App.tsx
@@ -1,22 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Application } from './components/application/application';
-import { Counter } from './components/counter/counter';
-
-// Testing Playground Extension
-// Testing playground extension can be found in google play store 
-// With the help of this extension we can find relevant query from the preview page itself
-// Simply install the extension > open the page ? open developers tools > select the element in page > switch to testing playground tab 
+import "./App.css";
+import { AppProviders } from "./providers/app-providers";
+import { MuiMode } from "./components/mui/mui-mode";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Application /> */}
-        <Counter />
-      </header>
-    </div>
+    <AppProviders>
+      <div className="App">
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
