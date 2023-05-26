7f83a9b955adaa8327b84bed2a6879c8f5d46cc3
faraz-github <farazahmad.email@gmail.com>
2023-05-26 23:58:02 +0530

    Use wrapper option for providers

--- reactjs_ts_test/src/components/mui/mui-mode.test.tsx
+++ reactjs_ts_test/src/components/mui/mui-mode.test.tsx
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { MuiMode } from "./mui-mode";
+import { AppProviders } from "../../providers/app-providers";
+
+// Now we will try testing components with providers
+// As they share information passed through providers 
+// Some tests can fail
 
 describe("MuiMode", () => {
   test("renders text correctly", () => {
-    render(<MuiMode />);
+    render(<MuiMode />, {
+        wrapper: AppProviders // With the help of wrapper option in render we can pass provider
+    });
     const headingElement = screen.getByRole("heading");
 
     expect(headingElement).toHaveTextContent("dark mode");
