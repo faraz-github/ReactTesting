67f73532fabb46c97fb028cba205ad243c78ca3b
faraz-github <farazahmad.email@gmail.com>
2023-05-28 23:43:03 +0530

    Setup msw and handlers for mocking api

---
+++ reactjs_ts_test/src/mocks/handlers.ts
@@ -0,0 +1,20 @@
+import { rest } from "msw"; // rest & graphql
+
+export const handlers = [
+  rest.get("https://jsonplaceholder.typeicode.com/users", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          name: "Bruce Wayne",
+        },
+        {
+          name: "Clark Kent",
+        },
+        {
+          name: "Princess Diana",
+        },
+      ])
+    );
+  }),
+];
